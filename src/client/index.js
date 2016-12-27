2220eb81c98c23a96a7ddffb05173aa6ce42c2c8
blivesta <design@blivesta.com>
2016-12-27 09:21:51 +0900

    Rename to Router.js from Routes.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,25 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import Root from 'Router'
+import Routes from './Routes'
 
 const rootElm = document.getElementById('root')
 
-ReactDOM.render(
+render(
   <AppContainer>
-    <Root />
+      <Routes />
   </AppContainer>,
   rootElm
 )
 
 if (module.hot) {
-  module.hot.accept('./Router', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./Router').default
-    ReactDOM.render(
+  module.hot.accept('./Routes', () => {
+    const HotRoutes = require('./Routes').default
+    render(
       <AppContainer>
-        <NextApp />
+          <HotRoutes />
       </AppContainer>,
       rootElm
     )
